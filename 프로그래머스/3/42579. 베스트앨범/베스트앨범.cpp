f37915d9baadeff07ddc149942d9cff2b1962417
potato_hun <107704298+potatohun@users.noreply.github.com>
2024-06-27 15:58:24 +0900

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -16,8 +16,9 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     for(int i = 0;i<genres.size();i++){ 
         genres_play_count[genres[i]] += plays[i]; // 장르별 총 플레이 수 정리
     }
+    
     for(int i = genres.size()-1; i >= 0 ;i--){
-        playlist[genres[i]].insert(pair<int,int>(plays[i], i));
+        playlist[genres[i]].insert(pair<int,int>(plays[i], i)); // 고유 번호는 내림 차순, 재생수는 오름차순으로 각 mulitmap에 저장
     }
     
     map<int, string> genres_play_count_sort; // 맵을 활용하여 오름차순으로 정렬
@@ -32,7 +33,7 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
         std::cout << iter->first << ":" << iter->second << std::endl;
         
         int twice = 0;
-        for(auto it = playlist[iter->second].rbegin(); it != playlist[iter->second].rend();it++) // 두 번만 출력 후 asnwer에 삽입
+        for(auto it = playlist[iter->second].rbegin(); it != playlist[iter->second].rend();it++) // 각 multimap에서 재생수의 내림차순, 고유 번호 오름 차순으로 출력
         {    
             answer.push_back(it->second);
             twice++;
