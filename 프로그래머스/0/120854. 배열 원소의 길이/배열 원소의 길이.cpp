9116c0e230e918e9f72f32b3f273ded0386a714d
potato_hun <107704298+potatohun@users.noreply.github.com>
2024-06-27 13:47:37 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer;
+    
+    for(int i = 0; i< strlist.size(); i++){
+        answer.push_back(strlist[i].length());
+    }
+    return answer;
+}
