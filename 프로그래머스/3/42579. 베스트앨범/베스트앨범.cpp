9c56da268ec22103e53cffb03a8263022eed1afb
potato_hun <107704298+potatohun@users.noreply.github.com>
2024-06-27 15:56:22 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+
+using namespace std;
+
+vector<int> solution(vector<string> genres, vector<int> plays) {
+    vector<int> answer;
+    
+    unordered_map<string, int> genres_play_count;
+    unordered_map<string, multimap<int, int>> playlist;
+    
+    for(int i = 0;i<genres.size();i++){ 
+        genres_play_count[genres[i]] += plays[i]; // 장르별 총 플레이 수 정리
+    }
+    for(int i = genres.size()-1; i >= 0 ;i--){
+        playlist[genres[i]].insert(pair<int,int>(plays[i], i));
+    }
+    
+    map<int, string> genres_play_count_sort; // 맵을 활용하여 오름차순으로 정렬
+    
+    for(auto iter = genres_play_count.begin(); iter != genres_play_count.end(); iter++)
+    {
+        genres_play_count_sort[iter->second] = iter-> first; // value를 기준으로 정렬
+    }
+    
+    for(auto iter = genres_play_count_sort.rbegin(); iter != genres_play_count_sort.rend(); iter++) // 큰 수 부터 출력 rbegin, rend 키워드 사용
+    {
+        std::cout << iter->first << ":" << iter->second << std::endl;
+        
+        int twice = 0;
+        for(auto it = playlist[iter->second].rbegin(); it != playlist[iter->second].rend();it++) // 두 번만 출력 후 asnwer에 삽입
+        {    
+            answer.push_back(it->second);
+            twice++;
+            if(twice==2) // 두번 입력 했을 경우 입력 종료
+                break;
+        }
+    }
+    
+    return answer;
+}
